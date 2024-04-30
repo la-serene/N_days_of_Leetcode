0c3c71b59cfb881de2076a3d0c7d848d8019210a
Nguyen Cao Ky <caoky2003xx@gmail.com>
2024-04-30 16:08:53 +0700

    That's all for 2day :/

---
+++ Leetcode75/Day 6/max_consecutive_3.py
@@ -0,0 +1,10 @@
+# 1004. Max Consecutive Ones III
+# Medium
+# https://leetcode.com/problems/max-consecutive-ones-iii/description/?envType=study-plan-v2&envId=leetcode-75
+
+"""
+Given a binary array nums and an integer k, return the maximum number of consecutive 1's in the array if you can flip
+at most k 0's.
+"""
+
+n = [0, 0, 1, 1, 0, 0, 1, 1, 1, 0, 1, 1, 0, 0, 0, 1, 1, 1, 1]
