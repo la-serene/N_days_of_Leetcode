6154d4033d8dbdfa8660f79011b37614e2c02a19
Nguyen Cao Ky <caoky2003xx@gmail.com>
2024-06-10 02:48:53 +0700

    add solution description

--- Daily Leetcode/Day 47/Thread/subarr_sum_equal_k.py
+++ Daily Leetcode/Day 47/Thread/subarr_sum_equal_k.py
@@ -9,6 +9,17 @@ equals to k.
 A subarray is a contiguous non-empty sequence of elements within an array.
 """
 
+"""
+Solution: If the prefix sum up to ith index is X, and the prefix sum up to jth index is Y and
+it is found that Y = X +k, then the required subarray is found with i as start index and j as end index.
+
+More specifically, since in mp we save the previous sum, by taking current sum - k then check if (sum - k) 
+present in mp, we know that in prev subarrs there are one or many subarr(s) that sums to (sum - k), so we will have
+n valid subarr.
+
+Like this:  [[] ... ] in which smaller [] sum to (sum - k) and large [] sum to sum, so large [] - small [] equal
+to k.
+"""
 
 class Solution:
     @staticmethod
