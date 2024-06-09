52060465dc108c3f5d07602d15ee3d8da2dc815b
Nguyen Cao Ky <caoky2003xx@gmail.com>
2024-06-10 03:26:52 +0700

    index by current sum remain by k

--- Daily Leetcode/Day 47/Thread/subarr_sum_divisible_by_k.py
+++ Daily Leetcode/Day 47/Thread/subarr_sum_divisible_by_k.py
@@ -9,3 +9,28 @@ sum divisible by k.
 A subarray is a contiguous part of an array.
 """
 
+"""
+Solution: Try Prefix Sum and Hashmap.
+
+Instead of using current sum as key, I use remain of sum at each iteration.
+Note that only increment a key's value after adding to count.
+"""
+
+
+class Solution:
+    def subarraysDivByK(self, nums, k: int) -> int:
+        mp = {0: 1}
+        cur_sum = 0
+        count = 0
+
+        for i in nums:
+            cur_sum += i
+            remain = cur_sum % k
+
+            if not mp.get(remain, 0) == 0:
+                count += mp[remain]
+                mp[remain] += 1
+            else:
+                mp[remain] = 1
+
+        return count
