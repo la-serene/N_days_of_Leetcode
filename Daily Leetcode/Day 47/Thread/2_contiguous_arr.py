69e793944b35970656c4fbd2889e9cc5b8c90454
Nguyen Cao Ky <caoky2003xx@gmail.com>
2024-06-10 22:53:04 +0700

    Day 47 daily challenge

---
+++ Daily Leetcode/Day 47/Thread/2_contiguous_arr.py
@@ -0,0 +1,9 @@
+# 525. Contiguous Array
+# Medium
+# https://leetcode.com/problems/contiguous-array/description/
+
+"""
+Given a binary array nums, return the maximum length of a contiguous subarray with an equal number of 0 and 1.
+"""
+
+
